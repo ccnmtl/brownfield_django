d44b3a08ea8a508dd8a7a0ef3daac99139c63db6
Cassandra Dunlop <cld2156@columbia.edu>
2014-09-11 13:26:55 -0400

    oh boy, very simple course view works

--- media/js/instructor.js
+++ media/js/instructor.js
@@ -36,26 +36,187 @@
     });
     
     
-//    var UserView = Backbone.View.extend({
-//        
-//        //el: '#container',
-//    	
-//        render: function () 
-//        {
-//            var html = "Backbone.js rocks!";
-//            this.$el.html(html);
-//            return this;
-//        }
-//    });
-//    	 //create an instance
-//    var userView = new UserView();
-//    console.log("Initializing a new user view for '" + userView + "'.");
-//    console.log("Initializing a new user view for '" + userView.name + "'.");
-//    $('#container').append(userView.render().el);
-//    //new UserView.render();
     
     
     
+    
+    //Attempting Views Again...
+    var CourseView = Backbone.View.extend({
+    	// You can stick template HTML directly in view as a variable
+    	// did not know that...
+    	template: _.template('Course Template <%= name %> <button class="del-crs"> Remove Course</button> '),
+    	//$container: null,
+    	
+    	events: {
+    		'click .del-crs' : 'onRemoveCourse'
+    	},
+    	
+        render: function () {
+            if (!this.model) 
+            {
+                throw "Model is not set for this view";
+            }
+            
+            var html = this.template(this.model.toJSON());
+            
+            this.$el.html(html);
+            return this;
+        },
+        
+    	onRemoveCourse: function()
+    	{
+    		//will add delete functionality later
+    		console.log("You tried to remove a course...");
+    	}
+    	
+    });// End CourseView
+
+    //Instantiate and see if it works...
+    //Works in Console...???
+    var test_crs = new CourseView({
+    	
+    	model: new Course({
+    		
+    	    name: 'Test Course Model In View'
+    	    
+    	})
+    	
+    });
+
+    
+    jQuery('.user_courses').append(test_crs.render().el);
+    
+    
+    
+
+ 	  
+//            initialize: function()//options)
+//            {
+//            	this.render();
+//               // _.bindAll(this, 'render', 'insert');
+//               // this.$container = options.$container;
+//               // this.listenTo(this.model, 'change', this.render);
+//               // this.insert();
+    //
+//            },
+//        	
+//            //render: function()
+//            //{
+//            //    this.$el.html(this.template(this.model.attributes));
+//            //    return this;
+//            //},   
+//            
+////            render: function(){
+////                //Pass variables in using Underscore.js Template
+////                var variables = { name: "Test Course Template Vars" };
+////                // Compile the template using underscore
+////                var template = _.template( $("#ctemp").html(), variables );
+////                // Load the compiled HTML into the Backbone "el"
+////                this.$el.html( template );
+////            },
+//            
+//            //insert: function()
+//            //{
+//            //    this.$container.append(this.$el);
+//            //}
+//            
+//            
+//            render: function(){
+//                this.$el.html('<li>' + this.model.get('name') + '</li>');
+    //
+//                // Returning the object is a good practice
+//                // that makes chaining possible
+//                return this;
+//            }
+    //
+//            
+//            
+//            
+
+    //
+    //
+//        var CourseListView = Backbone.View.extend({
+    //
+//            initialize: function() 
+//            {
+//                this.bbmain = $('#bb_main');
+    //
+//                this.listenTo(course_collection, 'change', this.render);
+    //
+//                course_collection.each(function(course){
+    //
+//                    var view = new CourseView({ model: course });
+//                    $('#bb_main').append(view.render().el);
+    //
+//                }, this);	// "this" is the context in the callback
+//                //_.bindAll(this, 'render');
+//                //console.log("Initializing a new CourseListView for '" + this + "'.");
+//            },
+//            
+//    		render: function() 
+//    		{
+//    		    var $container = this.$('#courselistview').empty();
+    //
+//    		    course_collection.each(function(course) 
+//    		    {
+//    		        new CourseView(
+//    		        {
+//    		            model: course,
+//    		            $container: $container
+//    		        }).render();
+//    			});
+    //
+//    		    return this;
+//    		    
+//    		 }//End render()
+//           
+//    	});// End CourseListView
+    //    
+    //    
+    //    
+    //    
+    //
+//        new CourseListView();
+    //
+    ////render: function(){
+    ////
+////        _.each(course_collection.getChecked(), function(elem){
+////            total += elem.get('price');
+////        });
+    ////
+////        // Update the total price
+////        this.total.text('$'+total);
+    ////
+////        return this;
+    ////}
+    ////
+//        var course_list = new CourseListView({
+//            el: $('#bb_main')
+//        });
+    //
+//        course_list.render();
+    //
+    //   // course_collection.on('add remove', function() {
+    //   //     course_list.render();
+    //   // });
+        
+
+        /*Testing Course View with very simple code from book
+         * */
+        
+//        var CourseView = Backbone.View.extend({
+//        	el: '#uniq_id_test'
+//            render: function () {
+//                var html = "Test Course Name Here";
+//                this.$el.html(html);
+//                return this;
+//            }
+//        });
+    //
+//        // create an instance
+//        //var courseView = new CourseView();
+//        //$('#uniq_id_test').append(courseView.render().el);
+//        new CourseView.render();
     
     
     
