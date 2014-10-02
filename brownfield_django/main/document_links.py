97fbd867cd8f303f8295d85ae77b03a68939ac12
Cassandra Dunlop <cld2156@columbia.edu>
2014-10-02 17:27:34 -0400

    just checking

---
+++ brownfield_django/main/document_links.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+
+NAME_ONE='''Video: Esker County Community Television: O'Ryan's Express'''
+LINK_ONE = '''<a href="javascript:openPopWin('{{STATIC_URL}}flvplayer.html?flv=oryans_express.flv&amp;title=Esker%20County%20Community%20Television:%20O,Ryan,s%20Express','500','500','no','no','bfaV1')">Video: Esker County Community Television: O'Ryan's Express</a>'''
+NAME_TWO='''Video: Press Conference Proceedings in Moraine Township'''
+LINK_TWO = '''<a href="javascript:openPopWin('{{STATIC_URL}}flvplayer.html?flv=news_conference.flv&amp;title=Press%20Conference%20Proceedings%20in%20Moraine%20Township','500','500','no','no','bfaV2')">Video: Press Conference Proceedings in Moraine Township</a>'''
+NAME_THREE='''News: Local Residents React to Prospect of Mall Plans'''
+LINK_THREE = '''<a href="javascript:openPopWin('{{STATIC_URL}}documents/news1.jpg')">News: Local Residents React to Prospect of Mall Plans</a>'''
+NAME_FOUR='''News: Water Testing Results Still Pending'''
+LINK_FOUR = '''a href="javascript:openPopWin('{{STATIC_URL}}documents/news2.jpg','430','950','no','no','bfaN2')">News: Water Testing Results Still Pending</a>'''
+NAME_FIVE='''News: Water Testing Reveal Contamination'''
+LINK_FIVE = '''<a href="javascript:openPopWin('{{STATIC_URL}}documents/news3.jpg','250','820','no','no','bfaN3')">News: Water Testing Reveal Contamination</a>'''
+NAME_SIX='''News: IRS Explores Terrain'''
+LINK_SIX = '''<a href="javascript:openPopWin('{{STATIC_URL}}documents/news4.jpg','260','900','no','no','bfaN4')">News: IRS Explores Terrain</a>'''
+NAME_SEVEN='''News: Doctor Still Haunted By Death of Patient'''
+LINK_SEVEN = '''<a href="javascript:openPopWin('{{STATIC_URL}}documents/news5.jpg','800','500','no','no','bfaN5')">News: Doctor Still Haunted By Death of Patient</a>'''
+NAME_EIGHT='''Link: Brownfield Action Reference Site'''
+LINK_EIGHT = '''<a href="javascript:openPopWin('http://brownfieldref.ccnmtl.columbia.edu/','945','600','yes','yes','bfaref')">Link: Brownfield Action Reference Site</a>'''
