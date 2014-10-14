ee4f5973afe7de405be92709c83fa2bfc40601e9
Cassandra Dunlop <cld2156@columbia.edu>
2014-10-14 12:49:00 -0400

    make array of students for submission

--- media/js/ccnmtljs/activate_course.js
+++ media/js/ccnmtljs/activate_course.js
@@ -1,71 +1,93 @@
+function get_students(){
+	var data = [];
+    jQuery('#student_team_tables tr').each(function(){
+        var student = {'pk': jQuery(this).find("td input[name='std-id']").val(),
+				       'first_name': jQuery(this).find("td input[name='first_name']").val(), 
+				       'last_name': jQuery(this).find("td input[name='last_name']").val(), 
+				       'email': jQuery(this).find("td input[name='email']").val(),
+				       'team': jQuery(this).find("td option:selected").text()
+                       } // end var student
+        data.push(student);
+		//console.log(student);
+    })
+    console.log(data);
+    return data;
+} //end get_students();
+
 jQuery(function() {
 
     jQuery('#activation-btn').on('click', function(e)
     {   
-    	//jQuery('#act-crs-frm').submit();
-    	jQuery(function() {
-             data = []
-             
-    	   	jQuery.ajax(
-    	    {
-    	        url: "/update_course/" + crs_id,
-    	    	type: "GET",
-    	    	success: function (data) 
-    	    	{
-    	    		var crs_data = data;
+    	jQuery(function()
+    	{
+            var data = get_students();
+            console.log(data);
+    	    //console.log(get_students());
+            //data.push(get_students());
+            //console.log(data);
+    		//get_students();
+    		console.log("on click after get students");
+	    }); // end inner on click function
+    	e.preventDefault(); //we don't want the form submitting
+	});// end on click function
+});
+//    	   	jQuery.ajax(
+//    	    {
+//    	        url: "/activate_course/" + crs_id,
+//    	    	type: "POST",
+//    	    	success: function (data) 
+//    	    	{
+//    	    		var crs_data = data;
+//    	    		console.log(data);
     	            //console.log(crs_data.course[0].professor);
-    	    	    jQuery('#id_name').val(crs_data.course[0].name);
-    	            jQuery('#id_startingBudget').val(crs_data.course[0].startingBudget);
-    	            jQuery('#id_enableNarrative').val(crs_data.course[0].enableNarrative);
-    	            jQuery('#id_message').val(crs_data.course[0].message);
-    	            jQuery('#id_active').val(crs_data.course[0].active);
-    	            jQuery('#id_archive').val(crs_data.course[0].archive);
-    	            jQuery('#id_professor option:selected' ).text(crs_data.course[0].professor);
+    	    	    //jQuery('#id_name').val(crs_data.course[0].name);
+    	            //jQuery('#id_startingBudget').val(crs_data.course[0].startingBudget);
+    	            //jQuery('#id_enableNarrative').val(crs_data.course[0].enableNarrative);
+    	            //jQuery('#id_message').val(crs_data.course[0].message);
+    	            //jQuery('#id_active').val(crs_data.course[0].active);
+    	            //jQuery('#id_archive').val(crs_data.course[0].archive);
+    	            //jQuery('#id_professor option:selected' ).text(crs_data.course[0].professor);
     	            
-    	        },
-    	    	           
-    	    	error: function(data) 
-    	    	{
-    	    	     console.log("There was an error getting course details.");
-    	        }
-    	    }); // end ajax GET
-    		
-    		
-    	    jQuery('#update-crs-btn').on('click', function(e)
-    	    {
-    	    	jQuery.ajax(
-    	        {
-    	            url: "/update_course/" + crs_id,
-    	            type: "POST",
-    	            data: {'name' : jQuery('#id_name').val(),
-    	            	   'startingBudget' : jQuery('#id_startingBudget').val(),
-    	            	   'enableNarrative' : jQuery('#id_enableNarrative').val(),
-    	            	   'message' : jQuery('#id_message').val(),
-    	            	   'active' : jQuery('#id_active').val(),
-    	            	   'archive' : jQuery('#id_archive').val(),
-    	            	   'professor' : jQuery('#id_professor option:selected' ).text()
-    	            	   },
-
-    	            success: function (data) 
-    	            {
-    	        		var crs_data = data;
-
-    	        	    jQuery('#id_name').val(crs_data.course[0].name);
-    	                jQuery('#id_startingBudget').val(crs_data.course[0].startingBudget);
-    	                jQuery('#id_enableNarrative').val(crs_data.course[0].enableNarrative);
-    	                jQuery('#id_message').val(crs_data.course[0].message);
-    	                jQuery('#id_active').val(crs_data.course[0].active);
-    	                jQuery('#id_archive').val(crs_data.course[0].archive);
-    	                jQuery('#id_professor option:selected' ).text(crs_data.course[0].professor);
-    	                
-    	            },
-    	                  
-    	            error: function(data) 
-    	            {
-    	                console.log("There was an error submitting your form.");
-    	            }
-    	          }); //end ajax UPDATE
-    	    	  e.preventDefault();
-    	    });// end update course on submit function
-    		
-    	});// end doc on ready
+//    	        },
+//    	    	           
+//    	    	error: function(data) 
+//    	    	{
+//    	    	     console.log("There was an error getting course details.");
+//    	        }
+//    	    }); // end ajax GET
+//    		
+//    		
+//    	    jQuery('#update-crs-btn').on('click', function(e)
+//    	    {
+//    	    	jQuery.ajax(
+//    	        {
+//    	            url: "/update_course/" + crs_id,
+//    	            type: "POST",
+//    	            data: {'name' : jQuery('#id_name').val(),
+//    	            	   'startingBudget' : jQuery('#id_startingBudget').val(),
+//    	            	   'enableNarrative' : jQuery('#id_enableNarrative').val(),
+//    	            	   'message' : jQuery('#id_message').val(),
+//    	            	   'active' : jQuery('#id_active').val(),
+//    	            	   'archive' : jQuery('#id_archive').val(),
+//    	            	   'professor' : jQuery('#id_professor option:selected' ).text()
+//    	            	   },
+//
+//    	            success: function (data) 
+//    	            {
+//    	        		var crs_data = data;
+//
+//    	        	    jQuery('#id_name').val(crs_data.course[0].name);
+//    	                jQuery('#id_startingBudget').val(crs_data.course[0].startingBudget);
+//    	                jQuery('#id_enableNarrative').val(crs_data.course[0].enableNarrative);
+//    	                jQuery('#id_message').val(crs_data.course[0].message);
+//    	                jQuery('#id_active').val(crs_data.course[0].active);
+//    	                jQuery('#id_archive').val(crs_data.course[0].archive);
+//    	                jQuery('#id_professor option:selected' ).text(crs_data.course[0].professor);
+//    	                
+//    	            },
+//    	                  
+//    	            error: function(data) 
+//    	            {
+//    	                console.log("There was an error submitting your form.");
+//    	            }
+//    	          }); //end ajax UPDATE
