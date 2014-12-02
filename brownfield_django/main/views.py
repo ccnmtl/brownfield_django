92e8c994c1e9b526f33fddcb59931967cf2584c0
Cassandra Dunlop <cdulop@columbia.com>
2014-12-02 17:30:52 -0500

    not sure why but it seems the instructor email template was lost

--- brownfield_django/main/views.py
+++ brownfield_django/main/views.py
@@ -172,7 +172,6 @@ class InstructorViewSet(viewsets.ModelViewSet):
 
     def send_instructor_email(self, instructor, profile):
         '''Send instructor their credentials'''
-        # print "inside send instructor email"
         template = loader.get_template(
             'main/ccnmtl/course_dash/instructor_activation_notice.txt')
         subject = "Welcome to Brownfield!"
@@ -433,7 +432,6 @@ class ActivateCourseView(CSRFExemptMixin, JSONResponseMixin, View):
 
     def post(self, request, pk):
         '''This is really really ugly as is get method need to clean up.'''
-        print "POST"
         student_list = json.loads(request.POST['student_list'])
         for student in student_list:
             team = Team.objects.get(pk=student['student']['team_id'])
