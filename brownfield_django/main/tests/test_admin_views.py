d382e076c3a7409aee285b8e374ab18f5b8258e8
Cassandra Dunlop <cld2156@columbia.edu>
2014-12-24 16:34:53 -0500

    flake clean up

--- brownfield_django/main/tests/test_admin_views.py
+++ brownfield_django/main/tests/test_admin_views.py
@@ -1,12 +1,11 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 from django.test.client import Client
 
 from factories import UserProfileFactory, UserFactory, \
-    CourseFactory, TeamFactory, DocumentFactory
+    CourseFactory, TeamFactory
 
 from rest_framework import status
 from rest_framework.test import APITestCase
-from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
 
 
@@ -39,7 +38,7 @@ class TestAdminViews(TestCase):
 
 class TestCourseRestViews(APITestCase):
     '''Test course related urls'''
- 
+
     def setUp(self):
         '''Courses ordered by name... setting now'''
         self.client = APIClient()
@@ -55,11 +54,10 @@ class TestCourseRestViews(APITestCase):
             professor=UserFactory(username='someuser'), name='CCourse')
         self.random_crs2 = CourseFactory(
             professor=UserFactory(username='someotheruser'), name='DCourse')
- 
+
     def test_get_courses_as_admin(self):
         ''' Get all Courses. '''
         self.client.login(username=self.admin.user.username, password="test")
-        
         response = self.client.get('/api/course/',
                                    format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -68,7 +66,7 @@ class TestCourseRestViews(APITestCase):
         self.assertEqual(response.data[1]['name'], self.teacher_crs.name)
         self.assertEqual(response.data[2]['name'], self.random_crs1.name)
         self.assertEqual(response.data[3]['name'], self.random_crs2.name)
- 
+
     def test_get_courses_as_teacher(self):
         ''' Get teachers courses. '''
         self.client.login(username=self.teacher.user.username, password="test")
@@ -82,7 +80,7 @@ class TestUserViewset(APITestCase):
     '''Test user viewset class, currently only admins should be returned
     as list of all users, everyone else should be returned their own user,
     if they are returned anything at all'''
- 
+
     def setUp(self):
         '''users ordered by username'''
         self.client = APIClient()
@@ -109,11 +107,11 @@ class TestUserViewset(APITestCase):
                          self.user_three.username)
         self.assertEqual(response.data[4]['username'],
                          self.teacher.user.username)
- 
- 
+
+
 class TestDocumentRestViews(APITestCase):
     '''Test document related urls'''
- 
+
     def setUp(self):
         self.client = APIClient()
         self.admin = UserProfileFactory(user=UserFactory(username='admin'),
@@ -126,16 +124,18 @@ class TestDocumentRestViews(APITestCase):
     def test_get_documents_as_teacher(self):
         ''' Get all Course Documents. '''
         self.client.login(username=self.teacher.user.username, password="test")
-        response = self.client.get('/api/document/?course=' + str(self.course.pk),
-                                   format='json')
+        response = self.client.get(
+            '/api/document/?course=' + str(self.course.pk), format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         '''By default courses have 8 documents'''
         self.assertEqual(len(response.data), 8)
- 
+
 #     def test_release_document_as_teacher(self):
 #         ''' Release a document. '''
-#         self.client.login(username=self.teacher.user.username, password="test")
-#         response = self.client.get('/api/document/?course=' + str(self.course.pk),
+#         self.client.login(username=self.teacher.user.username,
+# password="test")
+#         response = self.client.get('/api/document/?course=' +
+# str(self.course.pk),
 #                                    format='json')
 #         response = self.client.put(
 #             '/api/document/' + str(response.data[0]['id']) + '/',
@@ -151,12 +151,12 @@ class TestDocumentRestViews(APITestCase):
 #              'visible': response.data[0]['visible']}, format='json')
 #         self.assertEqual(response.status_code, status.HTTP_200_OK)
 #         self.assertEqual(response.data, False)
- 
- 
+
+
 class TestStudentRestViews(APITestCase):
     '''Test student related urls and methods, has GET, PUT/update,
     POST/create, DELETE/destroy'''
-  
+
     def setUp(self):
         ''' Admin will log in, and navigate to a course page. '''
         self.client = APIClient()
@@ -167,24 +167,28 @@ class TestStudentRestViews(APITestCase):
         self.course = CourseFactory(professor=self.teacher.user,
                                     name='TestCourse')
         self.populated_course = CourseFactory(professor=self.teacher.user,
-                                    name='TestCourse')
+                                              name='TestCourse')
         self.student_one = UserProfileFactory(
-            user=UserFactory(username='student_one', first_name='astudent',
-                             last_name='student_one', email='student_one@email.com'),
-                             profile_type='ST', course=self.populated_course)
+            user=UserFactory(
+                username='student_one', first_name='astudent',
+                last_name='student_one', email='student_one@email.com'),
+            profile_type='ST', course=self.populated_course)
         self.student_two = UserProfileFactory(
-            user=UserFactory(username='student_two', first_name='bstudent',
-                             last_name='student_two', email='student_two@email.com'),
-                                          profile_type='ST', course=self.populated_course)
+            user=UserFactory(
+                username='student_two', first_name='bstudent',
+                last_name='student_two', email='student_two@email.com'),
+            profile_type='ST', course=self.populated_course)
         self.student_three = UserProfileFactory(
-            user=UserFactory(username='student_three', first_name='cstudent',
-                             last_name='student_three', email='student_three@email.com'),
-                             profile_type='ST', course=self.populated_course)
+            user=UserFactory(
+                username='student_three', first_name='cstudent',
+                last_name='student_three', email='student_three@email.com'),
+            profile_type='ST', course=self.populated_course)
         self.student_four = UserProfileFactory(
-            user=UserFactory(username='student_four', first_name='dstudent',
-                             last_name='student_four', email='student_four@email.com'),
-                             profile_type='ST', course=self.populated_course)
-  
+            user=UserFactory(
+                username='student_four', first_name='dstudent',
+                last_name='student_four', email='student_four@email.com'),
+            profile_type='ST', course=self.populated_course)
+
     def test_get_students_as_teacher(self):
         ''' Any students in the course will be returned via GET '''
         self.client.login(username=self.teacher.user.username, password="test")
@@ -208,11 +212,12 @@ class TestStudentRestViews(APITestCase):
     def test_create_student_as_admin(self):
         ''' Testing that admin can create students for teacher's course.  '''
         self.client.login(username=self.admin.user.username, password="test")
-        response = self.client.post('/api/student/?course=' + str(self.course.pk),
-                                    {'first_name': 'Student First Name',
-                                     'last_name': 'Student Last Name',
-                                     'email': 'studentemail@email.com'},
-                                     format='json')
+        response = self.client.post(
+            '/api/student/?course=' + str(self.course.pk),
+            {'first_name': 'Student First Name',
+             'last_name': 'Student Last Name',
+             'email': 'studentemail@email.com'},
+            format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         '''After student is created, it should return the student data
         in response.data'''
@@ -224,11 +229,12 @@ class TestStudentRestViews(APITestCase):
     def test_create_student_as_teacher(self):
         ''' Testing that admin can create students for teacher's course.  '''
         self.client.login(username=self.teacher.user.username, password="test")
-        response = self.client.post('/api/student/?course=' + str(self.course.pk),
-                                    {'first_name': 'Student First Name',
-                                     'last_name': 'Student Last Name',
-                                     'email': 'studentemail@email.com'},
-                                     format='json')
+        response = self.client.post(
+            '/api/student/?course=' + str(self.course.pk),
+            {'first_name': 'Student First Name',
+             'last_name': 'Student Last Name',
+             'email': 'studentemail@email.com'},
+            format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         '''After student is created, it should return the student data
         in response.data'''
@@ -237,54 +243,68 @@ class TestStudentRestViews(APITestCase):
                                          'last_name': u'Student Last Name',
                                          'email': u'studentemail@email.com'})
 
-    def test_get_students_as_teacher(self):
+    def test_get_students_as_teacher_class(self):
         ''' Now trying get on populated course. '''
         self.client.login(username=self.teacher.user.username, password="test")
-        response = self.client.get('/api/student/?course=' +
-                                   str(self.populated_course.pk), format='json')
+        response = self.client.get(
+            '/api/student/?course=' +
+            str(self.populated_course.pk), format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         '''QuerySset is ordered by first name'''
         self.assertEqual(len(response.data), 4)
-        self.assertEqual(response.data[0]['first_name'], self.student_one.user.first_name)
-        self.assertEqual(response.data[1]['first_name'], self.student_two.user.first_name)
-        self.assertEqual(response.data[2]['first_name'], self.student_three.user.first_name)
-        self.assertEqual(response.data[3]['first_name'], self.student_four.user.first_name)
+        self.assertEqual(response.data[0]['first_name'],
+                         self.student_one.user.first_name)
+        self.assertEqual(response.data[1]['first_name'],
+                         self.student_two.user.first_name)
+        self.assertEqual(response.data[2]['first_name'],
+                         self.student_three.user.first_name)
+        self.assertEqual(response.data[3]['first_name'],
+                         self.student_four.user.first_name)
 
-    def test_get_students_as_admin(self):
+    def test_get_students_as_admin_class(self):
         ''' Now trying get on populated course. '''
         self.client.login(username=self.admin.user.username, password="test")
         response = self.client.get('/api/student/?course=' +
-                                   str(self.populated_course.pk), format='json')
+                                   str(self.populated_course.pk),
+                                   format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         '''QuerySset is ordered by first name'''
         self.assertEqual(len(response.data), 4)
-        self.assertEqual(response.data[0]['first_name'], self.student_one.user.first_name)
-        self.assertEqual(response.data[1]['first_name'], self.student_two.user.first_name)
-        self.assertEqual(response.data[2]['first_name'], self.student_three.user.first_name)
-        self.assertEqual(response.data[3]['first_name'], self.student_four.user.first_name)
+        self.assertEqual(response.data[0]['first_name'],
+                         self.student_one.user.first_name)
+        self.assertEqual(response.data[1]['first_name'],
+                         self.student_two.user.first_name)
+        self.assertEqual(response.data[2]['first_name'],
+                         self.student_three.user.first_name)
+        self.assertEqual(response.data[3]['first_name'],
+                         self.student_four.user.first_name)
 
     def test_update_student_as_admin(self):
         ''' Edit a student as admin '''
         self.client.login(username=self.admin.user.username, password="test")
         response = self.client.get('/api/student/?course=' +
-                                   str(self.populated_course.pk), format='json')
-        new_response = self.client.put('/api/student/'+ str(response.data[0]['id']) + '/',
-                                   {'first_name': 'Edit First Name',
-                                    'last_name': 'Edit Last Name',
-                                    'email': 'editmail@email.com'},
-                                    format='json')
+                                   str(self.populated_course.pk),
+                                   format='json')
+        new_response = self.client.put(
+            '/api/student/' + str(response.data[0]['id']) + '/',
+            {'first_name': 'Edit First Name',
+             'last_name': 'Edit Last Name',
+             'email': 'editmail@email.com'},
+            format='json')
         self.assertEqual(new_response.status_code, status.HTTP_200_OK)
-        
+
     def test_update_student_as_teacher(self):
         ''' Edit a student as teacher '''
         self.client.login(username=self.teacher.user.username, password="test")
         response = self.client.get('/api/student/?course=' +
-                                   str(self.populated_course.pk), format='json')
-        new_response = self.client.put('/api/student/'+ str(response.data[0]['id']) + '/',
-                                   {'first_name': 'Edit First Name',
-                                    'last_name': 'Edit Last Name',
-                                    'email': 'editmail@email.com'},
-                                    format='json')
+                                   str(self.populated_course.pk),
+                                   format='json')
+        new_response = self.client.put(
+            '/api/student/'+str(response.data[0]['id']) + '/',
+            {'first_name': 'Edit First Name',
+             'last_name': 'Edit Last Name',
+             'email': 'editmail@email.com'},
+            format='json')
         self.assertEqual(new_response.status_code, status.HTTP_200_OK)
 
     def test_delete_student_as_admin(self):
--- brownfield_django/main/tests/test_models.py
+++ brownfield_django/main/tests/test_models.py
@@ -1,72 +1,70 @@
-from datetime import date
- 
 from django.test import TestCase
- 
+
 from factories import UserFactory, UserProfileFactory, \
-    CourseFactory, HistoryFactory, TeamFactory, InformationTestFactory, \
-    PerformedTestFactory, DocumentFactory
+    CourseFactory, HistoryFactory, TeamFactory, \
+    PerformedTestFactory, InformationTestFactory
 
 
 '''Very basic model tests...'''
- 
- 
+
+
 class TestUserFactory(TestCase):
- 
+
     def test_unicode(self):
         user = UserFactory()
         self.assertEqual(str(user), user.username)
- 
- 
+
+
 class TestCourseFactory(TestCase):
- 
+
     def test_unicode(self):
         course = CourseFactory()
         self.assertEqual(str(course), course.name)
- 
+
     def test_get_student_users_empty(self):
         course = CourseFactory()
         r = course.get_student_users()
         self.assertEqual(r.count(), 0)
- 
- 
+
+
 class TestTeamFactory(TestCase):
- 
+
     def test_unicode(self):
         team = TeamFactory(user=UserFactory())
         self.assertEqual(str(team), team.user.username)
- 
- 
+
+
 class TestHistoryFactory(TestCase):
- 
+
     def test_unicode(self):
         his = HistoryFactory()
         self.assertEqual(str(his), '%s - %s' % (his.description, his.team))
- 
- 
+
+
 class TestPerformedTestFactory(TestCase):
- 
+
     def test_unicode(self):
         pt = PerformedTestFactory()
         self.assertEqual(str(pt), '%s - %s' % (pt.testDetails, pt.paramString))
- 
- 
+
+
 class TestInformationTestFactory(TestCase):
- 
+
     def test_unicode(self):
         inf_t = InformationTestFactory()
         self.assertEqual(str(inf_t),
                          '%s - %s' % (inf_t.infoType, inf_t.internalName))
- 
- 
+
+
 class TestUserProfileFactory(TestCase):
- 
+
     def test_unicode(self):
         up = UserProfileFactory()
         self.assertEqual(str(up), up.user.username)
- 
- 
+
+
 class TestAdminProfile(TestCase):
- 
+
     def test_unicode(self):
         admin = UserProfileFactory(user=UserFactory(username='admin'),
                                    profile_type='AD')
@@ -74,10 +72,10 @@ class TestAdminProfile(TestCase):
         self.assertEqual(admin.role(), "administrator")
         self.assertEqual(admin.is_admin(), True)
         self.assertEqual(admin.is_student(), False)
- 
- 
+
+
 class TestTeacherProfile(TestCase):
- 
+
     def test_unicode(self):
         teach = UserProfileFactory(user=UserFactory(username='teacher'),
                                    profile_type='TE')
@@ -85,36 +83,40 @@ class TestTeacherProfile(TestCase):
         self.assertEqual(teach.role(), "faculty")
         self.assertEqual(teach.is_teacher(), True)
         self.assertEqual(teach.is_student(), False)
- 
- 
+
+
 class TestStudentProfile(TestCase):
     '''The UserProfile Factory is a student so we will test that.'''
     def test_unicode(self):
         student = UserProfileFactory(user=UserFactory(username='student'),
-                                   profile_type='ST')
+                                     profile_type='ST')
         self.assertEqual(str(student), student.user.username)
         self.assertEqual(student.role(), "student")
         self.assertEqual(student.is_student(), True)
         self.assertEqual(student.is_admin(), False)
- 
- 
+
+
 class TestCourseMethods(TestCase):
     '''
     Course model has several helper methods to assist
     in retrieving data for the dashboard.
     '''
-    
+
     def setUp(self):
         self.course = CourseFactory(name="Course Methods")
-        self.student1 = UserProfileFactory(user=UserFactory(username='student1'),
-                                   profile_type='ST', course=self.course)
-        self.student2 = UserProfileFactory(user=UserFactory(username='student2'),
-                                   profile_type='ST', course=self.course)
-        self.student3 = UserProfileFactory(user=UserFactory(username='student3'),
-                                   profile_type='ST', course=self.course)
-        self.student4 = UserProfileFactory(user=UserFactory(username='student4'),
-                                   profile_type='ST', course=self.course)
- 
+        self.student1 = UserProfileFactory(
+            user=UserFactory(username='student1'),
+            profile_type='ST', course=self.course)
+        self.student2 = UserProfileFactory(
+            user=UserFactory(username='student2'),
+            profile_type='ST', course=self.course)
+        self.student3 = UserProfileFactory(
+            user=UserFactory(username='student3'),
+            profile_type='ST', course=self.course)
+        self.student4 = UserProfileFactory(
+            user=UserFactory(username='student4'),
+            profile_type='ST', course=self.course)
+
     def test_course_get_students(self):
         '''
         Make sure students are returned when get_students
@@ -124,7 +126,7 @@ class TestCourseMethods(TestCase):
         self.assertTrue(self.student2 in self.course.get_students())
         self.assertTrue(self.student3 in self.course.get_students())
         self.assertTrue(self.student4 in self.course.get_students())
- 
+
     def test_course_get_documents(self):
         '''Updated the Course model to add the documents when created
         instead of adding them in the view. Should have 8 documents.'''
@@ -132,15 +134,17 @@ class TestCourseMethods(TestCase):
 
 
 class TestTeamMethods(TestCase):
- 
+
     def setUp(self):
         '''Adding student profiles, and history records for testing here'''
         self.team = TeamFactory(user=UserFactory())
-        self.student1 = UserProfileFactory(user=UserFactory(username='student1'),
-                                   profile_type='ST', team=self.team)
-        self.student2 = UserProfileFactory(user=UserFactory(username='student2'),
-                                   profile_type='ST', team=self.team)
- 
+        self.student1 = UserProfileFactory(
+            user=UserFactory(username='student1'),
+            profile_type='ST', team=self.team)
+        self.student2 = UserProfileFactory(
+            user=UserFactory(username='student2'),
+            profile_type='ST', team=self.team)
+
     def test_get_team_members(self):
         self.assertTrue(self.student1 in self.team.get_team_members())
         self.assertTrue(self.student2 in self.team.get_team_members())
