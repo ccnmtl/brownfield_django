e22b091230ef99746473ed1dfcdc541c3b5581f6
Cassandra Dunlop <cld2156@columbia.edu>
2014-12-29 11:38:06 -0500

    silly mistake

--- brownfield_django/main/tests/__init__.py
+++ brownfield_django/main/tests/__init__.py
@@ -2,7 +2,6 @@
 from test_models import *
 from test_views import *
 from test_admin_views import *
-from test_team_views import *
 # from test_interactive import *
 # from test_views import *
 # from test_views import *
