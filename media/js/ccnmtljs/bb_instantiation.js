f6d746b871bacd479725301810a4890405a0ffcb
Cassandra Dunlop <cld2156@columbia.edu>
2015-01-07 13:59:32 -0500

    whoops assigned view to wrong element

--- media/js/ccnmtljs/bb_instantiation.js
+++ media/js/ccnmtljs/bb_instantiation.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function () {
 	var course = jQuery("input[name='crs-id']").val();
 
     var student_control_view = new StudentControlView({
-        el: jQuery('.team-creation-area'),
+        el: jQuery('.course-students'),
         course: course
     });
 
