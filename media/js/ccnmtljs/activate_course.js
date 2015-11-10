bcd7efa6aaa7e2234da96233c47206fb198f42f2
Anders Pearson <anders@columbia.edu>
2015-11-10 12:55:37 +0000

    some whitespace fixes

--- media/js/ccnmtljs/activate_course.js
+++ media/js/ccnmtljs/activate_course.js
@@ -45,27 +45,25 @@ function get_active_content(crs_id){
 
 function get_students(){
     //getting students from table
-	var data = [];
-	
-	jQuery('.student-row').each(
+    var data = [];
+    jQuery('.student-row').each(
         function(){
             var student = {'pk': jQuery(this).find("td input[name='std-id']").val(),
-			   'first_name': jQuery(this).find("td input[name='first_name']").val(), 
-		       'last_name': jQuery(this).find("td input[name='last_name']").val(), 
-			   'email': jQuery(this).find("td input[name='email']").val(),
-			   'team_id': jQuery(this).find("td option:selected").val(),
-			   'team_name': jQuery(this).find("td option:selected").text()
+                           'first_name': jQuery(this).find("td input[name='first_name']").val(),
+                           'last_name': jQuery(this).find("td input[name='last_name']").val(),
+                           'email': jQuery(this).find("td input[name='email']").val(),
+                           'team_id': jQuery(this).find("td option:selected").val(),
+                           'team_name': jQuery(this).find("td option:selected").text()
                };
-            
-        data.push({ 'student' : student });
-            
-    });
+
+            data.push({ 'student' : student });
+
+        });
 
     return data;
 }
 
 jQuery(function() {
-	
     var crs_id = jQuery("input[name='crs-id']").val();
     var activation_status = jQuery("input[name='course_active']").val();
 
@@ -82,84 +80,81 @@ jQuery(function() {
 
     }
 
-    jQuery('#get_teams').on('click', function(e)
-    {    
-         if(activation_status === "True"){
-             jQuery(".course-activation").load("/show_teams/" + crs_id + "/");
-         }
-         if(activation_status === "False"){
-             jQuery(".course-activation").load("/edit_teams/" + crs_id + "/");
-         }
-    });
+    jQuery('#get_teams')
+        .on('click', function(e)
+            {
+                if(activation_status === "True"){
+                    jQuery(".course-activation").load("/show_teams/" + crs_id + "/");
+                }
+                if(activation_status === "False"){
+                    jQuery(".course-activation").load("/edit_teams/" + crs_id + "/");
+                }
+            });
 
     /* button for activating/reactivating course */
-    jQuery('#activation-btn').on('click', function(e)
-    {   
-        if(activation_status === "True")
-	{
-            jQuery('#activation-btn').html("Save Changes");
-	    confirm_reactivation();
-        }
-		
-	if(activation_status === "False")
-	{
-            jQuery('#activation-btn').html("Activate Course");
-            jQuery('#confirmAct').modal('show');
-            confirm_activation();
-        }
-        e.preventDefault();
-    });
+    jQuery('#activation-btn')
+        .on('click', function(e)
+            {
+                if(activation_status === "True")
+                {
+                    jQuery('#activation-btn').html("Save Changes");
+                    confirm_reactivation();
+                }
+                if(activation_status === "False")
+                {
+                    jQuery('#activation-btn').html("Activate Course");
+                    jQuery('#confirmAct').modal('show');
+                    confirm_activation();
+                }
+                e.preventDefault();
+            });
 
     jQuery('#conf-act').on('click', function(e)
     {
         jQuery('#confirmAct').modal('hide');
-    	var student_list = get_students();
-    	var student_list_2 = JSON.stringify(student_list);
+        var student_list = get_students();
+        var student_list_2 = JSON.stringify(student_list);
 
         jQuery.ajax(
-    	{
-    	    url: "/activate_course/" + crs_id + "/",
-    	    type: "POST",
-    	    dataType: 'json',
-    	    data: {'student_list' : student_list_2},
-    	    success: function (data)
             {
-    	        if(activation_status === "True")
+                url: "/activate_course/" + crs_id + "/",
+                type: "POST",
+                dataType: 'json',
+                data: {'student_list' : student_list_2},
+                success: function (data)
                 {
-    		    reactivation_success();
-    		}
+                    if(activation_status === "True")
+                    {
+                        reactivation_success();
+                    }
 
-		    if(activation_status === "False")
-            {
-    		    activation_success();
-    		}
+                    if(activation_status === "False")
+                    {
+                        activation_success();
+                    }
 
-                jQuery("input[name='course_active']").val("True");
-                activation_status = jQuery("input[name='course_active']").val();
-                jQuery('#activation-btn').html("Re-Activate Course");
-    	    },
-    	    error: function(data) 
-    	    {
-    	        alert('Something went wrong, please try again');
-            }
-        });// end ajax
+                    jQuery("input[name='course_active']").val("True");
+                    activation_status = jQuery("input[name='course_active']").val();
+                    jQuery('#activation-btn').html("Re-Activate Course");
+                },
+                error: function(data)
+                {
+                    alert('Something went wrong, please try again');
+                }
+            });// end ajax
     });// end conf-act on click
 
-    
-    jQuery('#edit-team-members').on('click', function(e)
-    {   
-        get_edit_content(crs_id);
-        show_not_active();
-        jQuery('#edit-team-members').hide();
-	//jQuery('#edit-team-members').html("Save Changes");
-
-    });
+    jQuery('#edit-team-members')
+        .on('click', function(e)
+            {
+                get_edit_content(crs_id);
+                show_not_active();
+                jQuery('#edit-team-members').hide();
+            });
 
     jQuery('#show-teams').on('click', function(e)
-    {
-        get_active_content(crs_id);
-    });
+                             {
+                                 get_active_content(crs_id);
+                             });
 
 });// end outer function
-
-
